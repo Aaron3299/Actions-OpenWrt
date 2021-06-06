a78d016119ede1d0af155d11d915a68b889e5773
1287329 <61381370+1287329@users.noreply.github.com>
2021-06-06 09:59:11 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 #
 # Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
 # https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part1.sh
-# Description: OpenWrt DIY script part 1 (Before Update feeds)
-#
-
-# Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+#SSR
+git clone https://github.com/fw876/helloworld package/luci-app-ssr-plus
+#dnsfilter去广告
+#git clone https://github.com/garypang13/luci-app-dnsfilter package/luci-app-dnsfilter
+#网络速度测试
+#git clone https://github.com/sirpdboy/NetSpeedTest package/NetSpeedTest
+#网络监控中文版
+git clone https://github.com/sirpdboy/luci-app-netdata package/luci-app-netdata
