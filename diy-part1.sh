0febbd47744f9204aa73ce3a310293668c131608
1287329 <61381370+1287329@users.noreply.github.com>
2021-06-13 10:47:13 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,7 +13,7 @@ git clone https://github.com/garypang13/luci-app-dnsfilter package/luci-app-dnsf
 #网络监控中文版
 git clone https://github.com/sirpdboy/luci-app-netdata package/luci-app-netdata
 #adguardhome
-git clone https://github.com/1287329/AdGuardHome
-git clone https://github.com/rufengsuixing/luci-app-adguardhome ./package/diy/luci-app-adguardhome
+git clone https://github.com/1287329/AdGuardHome package/adguardHome
+git clone https://github.com/1287329/luci-app-adguardhome package/luci-app-adguardhome
 #filebrowser
-git clone https://github.com/xiaozhuai/luci-app-filebrowser ./package/diy/luci-app-filebrowser
+git clone https://github.com/xiaozhuai/luci-app-filebrowser package/diy/luci-app-filebrowser
