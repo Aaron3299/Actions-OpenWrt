d64205aede3eb30990fd6b99cd03c43aa5cbf6ab
1287329 <61381370+1287329@users.noreply.github.com>
2021-06-13 10:49:09 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -7,13 +7,14 @@
 #SSR
 git clone https://github.com/fw876/helloworld package/luci-app-ssr-plus
 #dnsfilter去广告
-git clone https://github.com/garypang13/luci-app-dnsfilter package/luci-app-dnsfilter
+#git clone https://github.com/garypang13/luci-app-dnsfilter package/luci-app-dnsfilter
 #网络速度测试
 #git clone https://github.com/sirpdboy/NetSpeedTest package/NetSpeedTest
 #网络监控中文版
 git clone https://github.com/sirpdboy/luci-app-netdata package/luci-app-netdata
 #adguardhome
 git clone https://github.com/1287329/AdGuardHome package/adguardHome
-git clone https://github.com/1287329/luci-app-adguardhome package/luci-app-adguardhome
+#git clone https://github.com/1287329/luci-app-adguardhome package/luci-app-adguardhome
+git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
 #filebrowser
 git clone https://github.com/xiaozhuai/luci-app-filebrowser package/diy/luci-app-filebrowser
