d5f068cdb3669e33c38a151ec1d3e889ec591740
1287329 <61381370+1287329@users.noreply.github.com>
2021-07-24 12:39:55 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-git clone https://github.com/fw876/helloworld package/luci-app-ssr-plus
+
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+echo 'src-git helloworld https://github.com/fw876/helloworld' >> feeds.conf.default
+echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >> feeds.conf.default
+#git clone https://github.com/fw876/helloworld package/luci-app-ssr-plus
 #adblock plus
 git clone https://github.com/small-5/luci-app-adblock-plus package/luci-app-adblock-plus
 #网络监控中文版
-git clone https://github.com/sirpdboy/luci-app-netdata package/luci-app-netdata
-#adguardhome
-agit clone https://github.com/1287329/AdGuardHome package/adguardHome
-#git clone https://github.com/1287329/luci-app-adguardhome package/luci-app-adguardhome
-git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
+#git clone https://github.com/sirpdboy/luci-app-netdata package/luci-app-netdata
