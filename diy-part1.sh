e30e0a930d5eab9e0095b49696bfb32a4ec5230a
1287329 <61381370+1287329@users.noreply.github.com>
2021-07-25 12:43:34 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 echo 'src-git helloworld https://github.com/fw876/helloworld' >> feeds.conf.default
-echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >> feeds.conf.default
+#echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >> feeds.conf.default
+#echo 'src-git lienol https://github.com/Lienol/openwrt-package' >> feeds.conf.default
+#liuran001软件源
+#git clone https://github.com/liuran001/openwrt-packages package
+#SSR
 #git clone https://github.com/fw876/helloworld package/luci-app-ssr-plus
+#smartdns
+git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
 #adblock plus
 git clone https://github.com/small-5/luci-app-adblock-plus package/luci-app-adblock-plus
 #网络监控中文版
-#git clone https://github.com/sirpdboy/luci-app-netdata package/luci-app-netdata
+git clone https://github.com/sirpdboy/luci-app-netdata package/luci-app-netdata
