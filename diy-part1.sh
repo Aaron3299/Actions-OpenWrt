142ab3ec90c95d7a065db547da6898aead4793a5
1287329 <61381370+1287329@users.noreply.github.com>
2022-06-04 10:02:08 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -5,9 +5,11 @@
 #
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
-echo 'src-git helloworld https://github.com/fw876/helloworld' >> feeds.conf.default
+#echo 'src-git helloworld https://github.com/fw876/helloworld' >> feeds.conf.default
 #echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >> feeds.conf.default
 #echo 'src-git lienol https://github.com/Lienol/openwrt-package' >> feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small' >> feeds.conf.default
+echo 'src-git 1287329 https://github.com/1287329/openwrt-diy' >> feeds.conf.default
 #liuran001软件源
 #git clone https://github.com/liuran001/openwrt-packages package
 #SSR
@@ -15,8 +17,8 @@ echo 'src-git helloworld https://github.com/fw876/helloworld' >> feeds.conf.defa
 #smartdns
 #git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
 #adguardhome
-git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
+#git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
 #网络监控中文版
-git clone https://github.com/sirpdboy/luci-app-netdata package/luci-app-netdata
+#git clone https://github.com/sirpdboy/luci-app-netdata package/luci-app-netdata
 #filebrowser
 #git clone https://github.com/xiaozhuai/luci-app-filebrowser package/luci-app-filebrowser
