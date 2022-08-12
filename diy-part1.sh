8b9d9b8258a2d287ff45c5fdbb5ddf9ce57eec4c
1287329 <61381370+1287329@users.noreply.github.com>
2022-08-13 04:05:05 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -8,7 +8,7 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >> feeds.conf.default
 #echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >> feeds.conf.default
 #echo 'src-git lienol https://github.com/Lienol/openwrt-package' >> feeds.conf.default
-#echo 'src-git small https://github.com/kenzok8/small' >> feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small' >> feeds.conf.default
 echo 'src-git 1287329 https://github.com/1287329/openwrt-diy' >> feeds.conf.default
 #liuran001软件源
 #git clone https://github.com/liuran001/openwrt-packages package
