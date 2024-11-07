e9c4015875e2eb99010d395d2b17795abc33a5d8
Aaron3299 <111169665+Aaron3299@users.noreply.github.com>
2024-11-07 08:05:27 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -5,7 +5,11 @@
 #
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
+cd openwrt
+rm -rf feeds/luci/applications/luci-app-transmission
+rm -rf feeds/packages/net/transmission
+rm -rf feeds/packages/net/transmission-web-control
+rm -rf feeds/packages/net/msd_lite
+rm -rf feeds/luci/applications/luci-app-msd_lite
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >> feeds.conf.default
-#svn export https://github.com/kiddin9/openwrt-packages/trunk/gowebdav package/diy/gowebdav
-#svn export https://github.com/kiddin9/openwrt-packages/trunk/luci-app-gowebdav package/diy/luci-app-gowebdav
-#svn export https://github.com/kiddin9/openwrt-packages/trunk/luci-app-adguardhome package/diy/luci-app-adguardhome
+echo 'src-git Aaron https://github.com/Aaron3299/opackage.git' >> feeds.conf.default
