0cc09f1bde1f37184469ba2b53533fee4fd1b579
Aaron3299 <111169665+Aaron3299@users.noreply.github.com>
2024-11-10 14:57:24 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -3,10 +3,6 @@
 # This is free software, licensed under the MIT License.
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
-rm -rf feeds/luci/applications/luci-app-transmission
-rm -rf feeds/packages/net/transmission
-rm -rf feeds/packages/net/transmission-web-control
-rm -rf feeds/packages/net/msd_lite
-rm -rf feeds/luci/applications/luci-app-msd_lite
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >> feeds.conf.default
-echo 'src-git Aaron https://github.com/Aaron3299/opackage.git' >> feeds.conf.default
+#echo 'src-git Aaron https://github.com/Aaron3299/opackage.git' >> feeds.conf.default
+git clone --depth 1 https://github.com/Aaron3299/opackage.git ./package/diy
