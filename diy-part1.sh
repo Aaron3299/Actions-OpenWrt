a9a7d5236b1b0b5c201472e4def999eb8fd47231
Aaron3299 <111169665+Aaron3299@users.noreply.github.com>
2024-11-11 12:39:38 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -5,4 +5,4 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >> feeds.conf.default
 #echo 'src-git Aaron https://github.com/Aaron3299/opackage.git' >> feeds.conf.default
-git clone --depth 1 https://github.com/Aaron3299/opackage.git ./package/diy
+#git clone --depth 1 https://github.com/Aaron3299/opackage.git ./package/diy
